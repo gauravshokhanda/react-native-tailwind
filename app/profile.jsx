d8e55d257f72653901e59e8f1787579f10bf1a97
Gaurav <gauravshokanda2@gmail.com>
2024-08-05 17:50:36 +0530

    first commit

---
+++ app/profile.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Profile = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Profile Page</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Profile;
